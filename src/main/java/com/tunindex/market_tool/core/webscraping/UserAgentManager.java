25b09839c28ae69150f8c331e56ec1c113f51d55
Fares Ben Slama <fares.benslama@esprit.tn>
2026-04-04 00:41:44 +0000

    first commit

--- src/main/java/com/tunindex/market_tool/core/webscraping/UserAgentManager.java
+++ src/main/java/com/tunindex/market_tool/core/webscraping/UserAgentManager.java
@@ -34,7 +34,9 @@ public class UserAgentManager {
         userAgents.add("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
         userAgents.add("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0");
         userAgents.add("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36");
-
+        userAgents.add("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
+        userAgents.add("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
+        userAgents.add("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36");
         log.info("Loaded {} user agents", userAgents.size());
     }
 

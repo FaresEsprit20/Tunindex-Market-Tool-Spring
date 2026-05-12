f1a6e7f8b08d95549c4be5e86bd00310e3df501e
Fares Ben Slama <fares.benslama@esprit.tn>
2026-05-12 19:08:27 +0000

    Spring Security Jwt added

--- api/src/main/java/com/tunindex/market_tool/api/services/two_facor/TwoFactorAuthServiceImpl.java
+++ api/src/main/java/com/tunindex/market_tool/api/services/two_facor/TwoFactorAuthServiceImpl.java
@@ -14,7 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
+import jakarta.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
 import java.time.Duration;
 import java.time.LocalDateTime;
